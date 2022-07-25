11c5c04f2b6293c36820ba1e8d8de6227f5c6850
Mister Zain <95512163+MrZainRP@users.noreply.github.com>
2022-07-26 01:13:23 +1000

    to work with mz-prison

--- client/main.lua
+++ client/main.lua
@@ -26,3 +26,32 @@ AddEventHandler('onResourceStart', function(resource)
 	  FetchSkills()
    end
 end)
+
+-------------------
+--HOSTILES NATIVE--
+-------------------
+
+RegisterNetEvent('mz-skills:client:SpawnShooters', function(position)
+    QBCore.Functions.TriggerCallback('mz-skills:server:SpawnShooters', function(netIds, position)
+        Wait(1000)
+        local ped = PlayerPedId()
+        for i=1, #netIds, 1 do
+            local npc = NetworkGetEntityFromNetworkId(netIds[i])
+            SetPedDropsWeaponsWhenDead(npc, false)
+            GiveWeaponToPed(npc, Config.PedGun2, 250, false, true)
+            SetPedMaxHealth(npc, 300)
+            SetPedArmour(npc, 200)
+            SetCanAttackFriendly(npc, true, false)
+            TaskCombatPed(npc, ped, 0, 16)
+            SetPedCombatAttributes(npc, 46, true)
+            SetPedCombatAttributes(npc, 0, false)
+            SetPedCombatAbility(npc, 100)
+            SetPedAsCop(npc, true)
+            SetPedRelationshipGroupHash(npc, `HATES_PLAYER`)
+            SetPedAccuracy(npc, 60)
+            SetPedFleeAttributes(npc, 0, 0)
+            SetPedKeepTask(npc, true)
+            SetBlockingOfNonTemporaryEvents(npc, true)
+        end
+    end, position)
+end)
