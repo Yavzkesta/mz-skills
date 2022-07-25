e9de44057865a681e513f41d1df24666f748e3e5
Mister Zain <95512163+MrZainRP@users.noreply.github.com>
2022-07-26 01:13:45 +1000

    to work with mz-prison

--- server/main.lua
+++ server/main.lua
@@ -2,7 +2,6 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateCallback('skillsystem:fetchStatus', function(source, cb)
 	local Player = QBCore.Functions.GetPlayer(source)
- 
 	if Player then
 		local status = MySQL.scalar.await('SELECT skills FROM players WHERE citizenid = ?', {Player.PlayerData.citizenid})
 		if status ~= nil then
@@ -13,9 +12,8 @@ QBCore.Functions.CreateCallback('skillsystem:fetchStatus', function(source, cb)
 	else
 		cb()
 	end
- end)
+end)
 
- 
 RegisterServerEvent('skillsystem:update')
 AddEventHandler('skillsystem:update', function (data)
      local Player = QBCore.Functions.GetPlayer(source)
@@ -23,4 +21,30 @@ AddEventHandler('skillsystem:update', function (data)
 		['@skills'] = data,
 		['@citizenid'] = Player.PlayerData.citizenid
 	})
+end)
+
+------------
+--HOSTILES--
+------------
+
+local peds = { 
+    `g_m_y_famca_01`,
+    `g_m_y_famdnf_01`,
+}
+
+local getRandomNPC = function()
+    return peds[math.random(#peds)]
+end
+
+QBCore.Functions.CreateCallback('mz-skills:server:SpawnShooters', function(source, cb, loc)
+    local netIds = {}
+    local netId
+    local npc
+    for i=1, #Config.Shooters['soldiers'].locations[loc].peds, 1 do
+        npc = CreatePed(30, getRandomNPC(), Config.Shooters['soldiers'].locations[loc].peds[i], true, false)
+        while not DoesEntityExist(npc) do Wait(10) end
+        netId = NetworkGetNetworkIdFromEntity(npc)
+        netIds[#netIds+1] = netId
+    end
+    cb(netIds)
 end)
